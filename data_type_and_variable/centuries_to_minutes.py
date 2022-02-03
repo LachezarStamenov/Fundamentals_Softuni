cc44b687749f0cb6c5dc0e935b7e90ee0d0caf6f
Lachezar <94559783+LachezarStamenov@users.noreply.github.com>
2022-02-03 09:56:35 +0200

    Add files via upload

---
+++ data_type_and_variable/centuries_to_minutes.py
@@ -0,0 +1,8 @@
+centuries = int(input())
+years = centuries * 100
+days = int(years * 365.2422)
+hours = (days * 24)
+minutes = (hours * 60)
+
+print(f"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes")
+# 5 centuries = 500 years = 182621 days = 4382904 hours = 262974240 minutes
