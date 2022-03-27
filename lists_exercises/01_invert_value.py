350515714752b40c29b3c13008d97b13970030de
Lachezar <94559783+LachezarStamenov@users.noreply.github.com>
2022-03-27 20:36:08 +0300

    Delete lists_exercises directory

--- lists_exercises/01_invert_value.py
+++
@@ -1,6 +0,0 @@
-string = input().split(' ')
-int_list = [int(element) for element in string]
-converted_list = [element * -1 for element in int_list]
-print(converted_list)
-
-# print([-int(el) for el in input().split()])
