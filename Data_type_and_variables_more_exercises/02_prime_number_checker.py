bc43b87429542d63ee432689d64ae4c8dc6cfdda
Lachezar <94559783+LachezarStamenov@users.noreply.github.com>
2022-03-27 20:32:55 +0300

    Delete Data_type_and_variables_more_exercises directory

--- Data_type_and_variables_more_exercises/02_prime_number_checker.py
+++
@@ -1,9 +0,0 @@
-def num_is_prime(num):
-    for n in range(2, int(num ** 1 / 2) + 1):
-        if num % n == 0:
-            return False
-    return True
-
-
-print(num_is_prime(int(input())))
-
--- Data_type_and_variables_more_exercises/03_decrypting_massages.py
+++
@@ -1,9 +0,0 @@
-key = int(input())
-n = int(input())
-massage = []
-for num in range(n):
-    letters = input()
-    new_letter = ord(letters) + key
-    massage.append(chr(new_letter))
-
-print("".join(massage))
