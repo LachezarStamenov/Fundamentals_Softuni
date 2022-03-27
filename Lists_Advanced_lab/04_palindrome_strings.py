57ca3be612ebef2ed81485d74d61d12bd5d3f904
Lachezar <94559783+LachezarStamenov@users.noreply.github.com>
2022-03-27 20:34:13 +0300

    Delete Lists_Advanced_lab directory

--- Lists_Advanced_lab/04_palindrome_strings.py
+++
@@ -1,7 +0,0 @@
-string = input().split(" ")  # read a string words from the user split with space and put them in a list
-palindrome_word = input()  # read a palindrome word from the user
-palindromes = [word for word in string if word == word[::-1]] # use a comprehension to find all the words which are
-# palindromes
-
-print(palindromes)
-print(f"Found palindrome {string.count(palindrome_word)} times")
--- Lists_Advanced_lab/05_sorting_namess.py
+++
@@ -1,3 +0,0 @@
-string = input().split(", ")
-sorted_list = sorted(string, key=lambda  x: (-len(x), x)) # Use a sorted() function to sort the names by their length first, and then - alphabetically
-print(sorted_list)
